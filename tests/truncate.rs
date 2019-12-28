26b6b3c3b3f2d694090e9d59356c919830dcb4e6
soya <ohnishi.soya@arc.cs.okayama-u.ac.jp>
2019-12-28 17:04:39 +0900

    Add some integration test

--- tests/truncate.rs
+++ tests/truncate.rs
@@ -1,6 +1,13 @@
-use bitwise::Bit;
+use bitwise::*;
 
 #[test]
 fn truncate_range() {
+    let a = Bit::new((10, 4));
+    assert_eq!(a.truncate((2, 0)), Bit::new(2));
+}
 
+#[test]
+fn truncate_bit() {
+    let a = Bit::new((10, 4));
+    assert_eq!(a.truncate(1), Bit::new(1));
 }
